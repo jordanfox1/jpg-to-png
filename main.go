be132a60e1e9f847ef5e07570ba0a476faf8cd23
jordanfox1 <jordanfox9415@gmail.com>
2023-10-19 19:43:56 +1000

    testing concurrency

--- main.go
+++ main.go
@@ -1,6 +1,57 @@
 package main
 
-func main() {
-	// convert all images in ./jpg to png format and save them in ./png
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+)
 
+func main() {
+	jpegFolder := "./jpg"
+	jpegFiles, err := readJPEGFiles(jpegFolder)
+	if err != nil {
+		fmt.Println("Error reading JPEG files:", err)
+		return
+	}
+
+	convertJPEGFileConcurrently(jpegFiles)
+}
+
+func readJPEGFiles(jpegFolder string) (map[string][]byte, error) {
+	jpegFiles := make(map[string][]byte)
+
+	err := filepath.Walk(jpegFolder, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && (filepath.Ext(info.Name()) == ".jpg" || filepath.Ext(info.Name()) == ".jpeg") {
+			jpegBytes, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			jpegFiles[path] = jpegBytes
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return jpegFiles, nil
+}
+
+func convertJPEGFileConcurrently(jpegFiles map[string][]byte) {
+	var wg sync.WaitGroup
+
+	for filename := range jpegFiles {
+		wg.Add(1)
+		go func(filename string) {
+			defer wg.Done()
+			fmt.Println("JPEG File:", filename)
+		}(filename)
+	}
+
+	wg.Wait()
 }
